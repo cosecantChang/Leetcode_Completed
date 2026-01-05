1729af17f60950856bdcbae995465dae1524ef69
cosecantChang <136778911+cosecantChang@users.noreply.github.com>
2026-01-05 15:14:08 +0800

    2124.c

---
+++ 2124.c
@@ -0,0 +1,4 @@
+bool checkIfPangram(char* sentence)
+{
+    return strchr(sentence,'a')&&strchr(sentence,'b')&&strchr(sentence,'c')&&strchr(sentence,'d')&&strchr(sentence,'e')&&strchr(sentence,'f')&&strchr(sentence,'g')&&strchr(sentence,'h')&&strchr(sentence,'i')&&strchr(sentence,'j')&&strchr(sentence,'k')&&strchr(sentence,'l')&&strchr(sentence,'m')&&strchr(sentence,'n')&&strchr(sentence,'o')&&strchr(sentence,'p')&&strchr(sentence,'q')&&strchr(sentence,'r')&&strchr(sentence,'s')&&strchr(sentence,'t')&&strchr(sentence,'u')&&strchr(sentence,'v')&&strchr(sentence,'w')&&strchr(sentence,'x')&&strchr(sentence,'y')&&strchr(sentence,'z');
+}
