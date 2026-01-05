481a3985c4ec434d02fc5182b6eaef0706ebeb79
cosecantChang <136778911+cosecantChang@users.noreply.github.com>
2026-01-05 14:56:01 +0800

    1.c

---
+++ 1.c
@@ -0,0 +1,19 @@
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    int i,j;
+    *returnSize=2;
+    int* ans=(int*)malloc(sizeof(int)*2);
+    for(i=0;i<numsSize;i++)
+    {
+        for(j=i+1;j<numsSize;j++)
+        {
+            if(nums[i]+nums[j]==target)
+            {
+                ans[0]=i;
+                ans[1]=j;
+                return ans;
+            }
+        }
+    }
+    return NULL;
+}
