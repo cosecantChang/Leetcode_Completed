b19d51216b6d3544c9bcd9db92794f8d517887c1
cosecantChang <136778911+cosecantChang@users.noreply.github.com>
2026-01-05 15:07:32 +0800

    434.c

---
+++ 434.c
@@ -0,0 +1,20 @@
+int countSegments(char* s)
+{
+    int i,count=0,flag=0;
+    for(i=0;i<strlen(s);i++)
+    {
+        if(s[i]==' ')
+        {
+            if(flag==1)
+            {
+                count++;
+                flag=0;
+            }
+        }
+        else
+        {
+            flag=1;
+        }
+    }
+    return count+flag;
+}
