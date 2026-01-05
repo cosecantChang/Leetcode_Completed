a2184f43540c92d3060fa2ccee77b1a0283680e9
cosecantChang <136778911+cosecantChang@users.noreply.github.com>
2026-01-05 15:02:40 +0800

    283.c

---
+++ 283.c
@@ -0,0 +1,19 @@
+void moveZeroes(int* nums,int numsSize)
+{
+    int i,count=0;
+    for(i=0;i<numsSize;i++)
+    {
+        if(nums[i]!=0)
+        {
+            nums[i-count] = nums[i];
+        }
+        else
+        {
+            count++;
+        }
+    }
+    for(i=numsSize-count;i<numsSize;i++)
+    {
+        nums[i]=0;
+    }
+}
